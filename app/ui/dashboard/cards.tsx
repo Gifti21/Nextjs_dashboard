c8cd33ea0ecee470d8c72b5733d853d9a74c4f71
Gifti21 <medhanit212121@gmail.com>
2025-08-01 16:42:07 +0300

    commit0

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -33,7 +33,7 @@ export default async function CardWrapper() {
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      /> 
     </>
   );
 }
